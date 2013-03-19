531134389ccf942789ba3e5cb537196a249b0b89
martin-rohwedder <rohwedder@hotmail.dk>
2013-03-19 18:52:28 +0100

    user entity unused imports removed

--- src/main/java/dk/movstream/web/domain/User.java
+++ src/main/java/dk/movstream/web/domain/User.java
@@ -1,15 +1,12 @@
 package dk.movstream.web.domain;
 
 import java.io.Serializable;
-import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.persistence.Version;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
