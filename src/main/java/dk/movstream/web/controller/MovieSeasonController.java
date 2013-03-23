5164d6e2233d08445dcdc153f4c7fa499de34dda
Martin Rohwedder <rohwedder@hotmail.dk>
2013-03-23 12:37:29 +0100

    show episodes view added.

---
+++ src/main/java/dk/movstream/web/controller/MovieSeasonController.java
@@ -0,0 +1,29 @@
+package dk.movstream.web.controller;
+
+import dk.movstream.web.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+/**
+ *
+ * @author martin
+ * @since 23-03-2013
+ * @version 1.0
+ */
+@Controller
+public class MovieSeasonController {
+
+    @Autowired
+    private MovieService movieService;
+    
+    @RequestMapping(value = {"/season/showepisodes/{seasonId}"}, method = RequestMethod.GET)
+    public ModelAndView renderSeasonEpisodes(@PathVariable("seasonId") long seasonId) {
+        ModelAndView mav = new ModelAndView("showepisodes");
+        
+        return mav;
+    }
+    
+} //End of class MovieSeasonController
---
+++ src/main/webapp/views/private/showepisodes.jsp
@@ -0,0 +1,1 @@
+<h1>Show Episodes</h1>
